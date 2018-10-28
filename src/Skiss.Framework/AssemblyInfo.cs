2f6df4d53fab3a36ba902e0c2b136eeb3e4dbccd
Simon Wendel <mail@simonwendel.se>
2018-10-28 18:24:56 +0100

    chore: remove framework code and add package refs to nuget
    Removes use of packages.config files in favor of refs in csprojs. A
    few minor package version upgrades may slip in.

--- src/Skiss.Framework/AssemblyInfo.cs
+++
@@ -1,7 +0,0 @@
-﻿// Copyright (c) Simon Wendel. All rights reserved.
-
-using System.Reflection;
-using System.Runtime.CompilerServices;
-
-[assembly: AssemblyCopyright("Copyright © Simon Wendel 2018")]
-[assembly: InternalsVisibleTo("Skiss.Framework.Tests")]
